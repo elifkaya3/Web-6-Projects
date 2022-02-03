bd07f6e4a12edd761b01b9f10417814a0a7a23dc
Web ve Mobil Prog <Web ve Mobil Prog@DESKTOP-8M7D7GE>
2022-02-03 14:55:56 +0300

    dizi

---
+++ Proje_09_Dizi/Proje_09_Dizi/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace Proje_09_Dizi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[] firstArray = {40,80,60};
+            for (int i = 0; i < 3; i++)
+            {
+            Console.WriteLine(firstArray[i]);
+            Console.ReadLine();
+
+            }*/
+
+            //rastgele sayı böyle üretlir  Random rnd = new Random();
+            // int rastgele = rnd.Next(1,100);
+
+            /* Random rstgl = new Random();
+             int[] sayilar=new int[10];
+             for (int i = 1; i < 10; i++)
+             {
+                 sayilar[i]=(rstgl.Next(1,100));
+             }
+             *//* for (int i = 0; i < 10; i++)
+              {
+                  Console.WriteLine(sayilar[i]*2);
+              }*//*
+             foreach (var sayi in sayilar)
+             {
+                 Console.WriteLine(sayi);
+             }
+             Console.ReadLine();*/
+
+            //programımız 1 ile 100 arasında rastgele bir sayı üretsin
+            //kullanıcıdan bu sayıyı atahmin etmesini isteyelim
+            //bilirse tebrik edelim bilemezse kaybettin diyelim ve bitirelim
+
+            /* Random rnd = new Random();
+             int sayi = rnd.Next(1,100);
+             Console.WriteLine($"{sayi}1 ile 100 arasında bir sayı giriniz:");
+             int kullaniciTahmin = int.Parse(Console.ReadLine());
+
+               if (sayi == kullaniciTahmin)
+                 {
+                     Console.WriteLine("tebrikler");
+                 }
+                 else
+                 {
+                     Console.WriteLine("kaybettiniz");
+                 }
+            
+             Console.ReadLine();*/
+            /* Random rnd = new Random();
+             int sayi = rnd.Next(1, 100);
+             Console.WriteLine($"{sayi}--1 ile 100 arasında 5 adet sayı giriniz:");
+             int girilecekSayi = 1;
+
+             while (girilecekSayi<=5)
+             {
+                 Console.WriteLine($"{girilecekSayi}.sayi" );
+                 int kullaniciTahmin = int.Parse(Console.ReadLine());
+                 girilecekSayi++;
+
+                 if (sayi == kullaniciTahmin)
+                 {
+                     Console.WriteLine("tebrikler oyun bitti");
+                     break;
+                 }
+                 else
+                 {
+                     Console.WriteLine("yanlış tahmin");
+
+                 }
+             }
+             Console.WriteLine("kaybettiniz!");
+             Console.ReadLine();*/
+
+
+            /* Random rnd = new Random();
+             int sayi = rnd.Next(1, 100);
+             int tahmin = 0;
+             int sayac = 1;
+             do
+             {    Console.WriteLine($"{sayac}.sayi ");
+                  tahmin = int.Parse(Console.ReadLine());
+                 if (sayi!=tahmin)
+                 {
+                     Console.WriteLine("yanlış tahmin");
+                 }
+
+                 sayac++;
+             } while (sayac<=5 && sayi!=tahmin);
+             if (sayi == tahmin)
+             {
+                 Console.WriteLine("tebrikler");
+             }
+             else
+             {
+                 Console.WriteLine("oyun bitti");
+             }
+             Console.ReadLine();*/
+
+
+            Random rnd = new Random();
+            int sayi = rnd.Next(1, 100);
+            Console.WriteLine($"{sayi}--1 ile 100 arasında 5 adet sayı giriniz:");
+            int girilecekSayi = 0;
+            
+            while (girilecekSayi <= 4)
+            {
+                Console.WriteLine($"{girilecekSayi+1}.sayi");
+                int kullaniciTahmin = int.Parse(Console.ReadLine());
+                girilecekSayi++;
+
+                if (sayi == kullaniciTahmin)
+                {
+                    Console.WriteLine($"{girilecekSayi}.sayi+{girilecekSayi*10}");
+
+                    Console.WriteLine("tebrikler oyun bitti");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("yanlış tahmin");
+
+                }
+                Console.WriteLine("kaybettiniz!");
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+}

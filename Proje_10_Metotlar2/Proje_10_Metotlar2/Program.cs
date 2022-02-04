bef25e52aa8099ac6b35f0104819f0bcfc92012d
Web ve Mobil Prog <Web ve Mobil Prog@DESKTOP-8M7D7GE>
2022-02-04 10:36:59 +0300

    metotlar2

---
+++ Proje_10_Metotlar2/Proje_10_Metotlar2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Proje_10_Metotlar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int Topla(int sayi1,int sayi2)// bir metodun imzası nedir:::
+                                          // 1-metodun adı,2- metodun parametre sayısı
+                                          // 3- metodun parametrelerinin tipleri
+            {
+              int sonuc = sayi1 + sayi2;
+              return sonuc;
+
+            }
+            Topla(50, 60); 
+            Console.WriteLine(Topla(50, 60));
+            Console.WriteLine(Topla(Topla(Topla(50, 60), 70),90));
+            Console.ReadLine();*/
+           
+
+        }
+    }
+}
